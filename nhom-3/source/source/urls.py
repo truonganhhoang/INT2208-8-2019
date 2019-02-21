33b67a995e412f4de6c5ab9496107a31ff160a74
Le Ba Cong <conglb@yahoo.com>
2019-02-21 10:00:47 +0700

    Update homepage's template

--- nhom-3/source/source/urls.py
+++ nhom-3/source/source/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hello.urls')),
+    path('', include('homepage.views')),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
