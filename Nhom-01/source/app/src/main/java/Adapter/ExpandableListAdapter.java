5ad5fb2093cee2b70a36982ac9e19cf6b0d6e266
Long Lee <longvuong9901@gmail.com>
2019-02-26 23:24:29 +0700

    add Expandable Listview

---
+++ Nhom-01/source/app/src/main/java/Adapter/ExpandableListAdapter.java
@@ -0,0 +1,93 @@
+package Adapter;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import com.example.jinny.vocabulary.R;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+    private Context context;
+    private List<String> listData;
+    private HashMap<String,List<String>> listChild;
+
+    public ExpandableListAdapter(Context context, List<String> listData, HashMap<String, List<String>> listHashMap) {
+        this.context = context;
+        this.listData = listData;
+        this.listChild = listHashMap;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return listData.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return listChild.get(listData.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return listData.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return listChild.get(listData.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        String headerTilte = (String)getGroup(groupPosition);
+        if (convertView==null){
+            LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_group,null);
+
+        }
+        TextView lbListHeader = (TextView) convertView.findViewById(R.id.lvlListHeader);
+        lbListHeader.setTypeface(null,Typeface.BOLD);
+        lbListHeader.setText(headerTilte);
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        final String childText = (String) getChild(groupPosition,childPosition);
+        if (convertView==null)
+        {
+            LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_item,null);
+        }
+        TextView txtListChild = (TextView) convertView.findViewById(R.id.lblListItem);
+        txtListChild.setText(childText);
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
--- Nhom-01/source/app/src/main/java/com/example/jinny/vocabulary/screen/MainActivity.java
+++ Nhom-01/source/app/src/main/java/com/example/jinny/vocabulary/screen/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.jinny.vocabulary.screen;
 
+import android.os.Bundle;
 import android.util.Log;
+import android.widget.ExpandableListView;
 
 import com.example.jinny.vocabulary.R;
 import com.example.jinny.vocabulary.base.BaseActivity;
@@ -8,7 +10,11 @@ import com.example.jinny.vocabulary.database.DatabaseManager;
 import com.example.jinny.vocabulary.model.Category;
 import com.example.jinny.vocabulary.model.Topic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import Adapter.ExpandableListAdapter;
 
 public class MainActivity extends BaseActivity {
 
@@ -17,6 +23,11 @@ public class MainActivity extends BaseActivity {
     private DatabaseManager databaseManager;
     private List<Topic> topics;
     private List<Category> categories;
+
+    private ExpandableListView listView;
+    private ExpandableListAdapter listAdapter;
+    private List<String> listDataHeader;
+    private HashMap<String,List<String>> listHashMap;
 
     @Override
     protected int getLayoutId() {
@@ -29,5 +40,45 @@ public class MainActivity extends BaseActivity {
         databaseManager = DatabaseManager.getInstance(this);
         topics = databaseManager.getListTopic();
         categories = databaseManager.getListCategory(topics);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ExpandableListView) findViewById(R.id.lvExp);
+        initData();
+        listAdapter = new ExpandableListAdapter(this,listDataHeader,listHashMap);
+        listView.setAdapter(listAdapter);
+    }
+
+    private void initData(){
+        listDataHeader = new ArrayList<>();
+        listHashMap = new HashMap<>();
+
+        listDataHeader.add("Business");
+        listDataHeader.add("Office");
+        listDataHeader.add("Personal");
+
+        List<String> business = new ArrayList<>();
+        business.add("Contracts");
+        business.add("Marketing");
+        business.add("Conference");
+
+        List<String> office = new ArrayList<>();
+        office.add("office1");
+        office.add("office2");
+        office.add("office3");
+
+        List<String> personal = new ArrayList<>();
+        personal.add("p1");
+        personal.add("p2");
+        personal.add("p3");
+
+        listHashMap.put(listDataHeader.get(0),business);
+        listHashMap.put(listDataHeader.get(1),office);
+        listHashMap.put(listDataHeader.get(2),personal);
+
     }
 }
