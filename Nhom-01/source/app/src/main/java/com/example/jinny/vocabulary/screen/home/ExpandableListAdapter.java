efedd08bdd64235cd0a3bb871edd3a5ba5233ff7
Long Lee <longvuong9901@gmail.com>
2019-03-04 20:39:49 +0700

    update
    - Click topic can show random word and the word detail from that topic
    - set Text of listTopicTextView

--- Nhom-01/source/app/src/main/java/com/example/jinny/vocabulary/screen/home/ExpandableListAdapter.java
+++ Nhom-01/source/app/src/main/java/com/example/jinny/vocabulary/screen/home/ExpandableListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.jinny.vocabulary.screen.home;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.view.LayoutInflater;
@@ -13,6 +14,8 @@ import android.widget.TextView;
 import com.example.jinny.vocabulary.R;
 import com.example.jinny.vocabulary.model.Category;
 import com.example.jinny.vocabulary.model.Topic;
+import com.example.jinny.vocabulary.screen.setting.SettingActivity;
+import com.example.jinny.vocabulary.screen.study.StudyActivity;
 
 import java.util.HashMap;
 import java.util.List;
@@ -66,14 +69,26 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
     @Override
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
         Category category = listData.get(groupPosition);
+        List<Topic> topics = listChild.get(listData.get(groupPosition).getName());
+
         if (convertView==null){
             LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.category_item,null);
         }
 
+        TextView listTopicsTextView = convertView.findViewById(R.id.list_topics);
         TextView lbListHeader = convertView.findViewById(R.id.lvlListHeader);
         LinearLayout llCategory = convertView.findViewById(R.id.ll_category);
 
+        //set Text cho TextView mô tả Category
+        String listTopics ="";
+        for (Topic topic : topics){
+            listTopics = listTopics + topic.getName() + ", ";
+        }
+        listTopics = listTopics.substring(0,listTopics.length()-2);
+        listTopicsTextView.setText(listTopics);
+
+        //set Text cho Category
         lbListHeader.setTypeface(null,Typeface.BOLD);
         lbListHeader.setText(category.getName());
         llCategory.setBackgroundColor(Color.parseColor(category.getColor()));
@@ -84,6 +99,7 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
         Topic topic = listChild.get(listData.get(groupPosition).getName()).get(childPosition);
+
         if (convertView==null)
         {
             LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -91,6 +107,16 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         }
         TextView txtListChild = convertView.findViewById(R.id.lblListItem);
         txtListChild.setText(topic.getName());
+        final Intent intent = new Intent(context.getApplicationContext() , StudyActivity.class);
+        intent.putExtra("topicID",topic.getId());
+        intent.putExtra("topicName",topic.getName());
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                context.startActivity(intent);
+            }
+        });
+
         return convertView;
     }
 
