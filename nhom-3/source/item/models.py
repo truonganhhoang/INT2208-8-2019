a6437ad860a66939e99bf716f5a395db692d8dfb
Le Ba Cong <conglb@yahoo.com>
2019-03-07 21:21:42 +0700

    Database: migrate Comment model

--- nhom-3/source/item/models.py
+++ nhom-3/source/item/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-
-
+class Comment(models.Model):
+    userId = models.ForeignKey(User, unique=True, on_delete=models.CASCADE)
+    content = models.CharField
 
 class Item(models.Model):
     name = models.CharField(max_length=50)
@@ -13,3 +15,4 @@ class Item(models.Model):
 
     def __str__(self):
         return name
+
