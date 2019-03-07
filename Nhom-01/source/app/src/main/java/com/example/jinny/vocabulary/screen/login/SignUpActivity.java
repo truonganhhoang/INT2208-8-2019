1944c2a10631893135070d55a3d162d2bf19c2b6
thienanh1999 <thienanh313@gmail.com>
2019-03-07 08:50:18 -0800

    feat: create signup activity and user setting

---
+++ Nhom-01/source/app/src/main/java/com/example/jinny/vocabulary/screen/login/SignUpActivity.java
@@ -0,0 +1,19 @@
+package com.example.jinny.vocabulary.screen.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.jinny.vocabulary.R;
+import com.example.jinny.vocabulary.base.BaseActivity;
+
+public class SignUpActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_sign_up;
+    }
+
+    @Override
+    protected void setupUI() {
+
+    }
+}
