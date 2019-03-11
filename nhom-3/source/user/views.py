ca56a50d1d2ce38e047a83d2e3e1df5821510ffc
Le Ba Cong <conglb@yahoo.com>
2019-03-11 09:53:43 +0700

    Add restaurant register route

--- nhom-3/source/user/views.py
+++ nhom-3/source/user/views.py
@@ -3,6 +3,9 @@ from django.urls import path, reverse
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate
+from django.views.generic import CreateView
+from .models import *
+from .form import *
 # Create your views here.
 
 def login_auth(request):
@@ -17,19 +20,40 @@ def login_auth(request):
 
 def login(request):
     template = loader.get_template('login.html')
-    HttpResponse(template)
+    return HttpResponse(template)
 
-def register():
+def register(request):
     template = loader.get_template('register.html')
-    HttpResponse(template)
+    return HttpResponse(template)
 
 def logout(request):
     logout(request)
     return HttpResponseRedirect(reverse("homepage"))
 
+class CustomerSignUpView(CreateView):
+    model = Customer
+    form_class = CustomerSignUpForm
+    template_name = "register.html"
+
+
+class RestaurantSignUpView(CreateView):
+    model = Restaurant
+    form_class = RestaurantSignUpForm
+    template_name = "register.html"
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'restaurant'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('homepage')
+
 
 urlpatterns = [
     path("login", login, name="login"),
-    path("register", register, name="register")
-    path("logout", logout, name="logout")
+    path("register", CustomerSignUpView.as_view() ,register, name="register"),
+    path("logout", logout, name="logout"),
+    path("register/restaurant", RestaurantSignUpView.as_view(), name="register_restauran"),
 ]
