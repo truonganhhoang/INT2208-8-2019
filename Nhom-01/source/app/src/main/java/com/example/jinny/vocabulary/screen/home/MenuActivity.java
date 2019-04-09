6c39b804e0523a5fd76d9ab095a87ff2357e1816
Long Lee <longvuong9901@gmail.com>
2019-04-09 11:09:45 +0700

    add activites: Menu and Quiz. Set up UI for them

---
+++ Nhom-01/source/app/src/main/java/com/example/jinny/vocabulary/screen/home/MenuActivity.java
@@ -0,0 +1,17 @@
+package com.example.jinny.vocabulary.screen.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.jinny.vocabulary.R;
+import com.example.jinny.vocabulary.screen.quiz.QuizActivity;
+import com.example.jinny.vocabulary.screen.study.StudyActivity;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
