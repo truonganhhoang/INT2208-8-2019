553cf9c140801bb77effff771e5f237cd2d83810
Long Lee <longvuong9901@gmail.com>
2019-04-09 11:21:13 +0700

    add logic of activities: Menu and Quiz

--- Nhom-01/source/app/src/main/java/com/example/jinny/vocabulary/screen/home/MenuActivity.java
+++ Nhom-01/source/app/src/main/java/com/example/jinny/vocabulary/screen/home/MenuActivity.java
@@ -2,16 +2,36 @@ package com.example.jinny.vocabulary.screen.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import com.example.jinny.vocabulary.R;
 import com.example.jinny.vocabulary.screen.quiz.QuizActivity;
-import com.example.jinny.vocabulary.screen.study.StudyActivity;
-
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        //set on click
+        findViewById(R.id.study_textview).setOnClickListener(this);
+        findViewById(R.id.quiz_textview).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()){
+            case R.id.study_textview:
+                intent = new Intent(MenuActivity.this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.quiz_textview:
+                intent = new Intent(MenuActivity.this, QuizActivity.class);
+                startActivity(intent);
+                break;
+
+        }
     }
 }
--- Nhom-01/source/app/src/main/java/com/example/jinny/vocabulary/screen/study/StudyActivity.java
+++ Nhom-01/source/app/src/main/java/com/example/jinny/vocabulary/screen/study/StudyActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.jinny.vocabulary.R;
 import com.example.jinny.vocabulary.base.BaseActivity;
@@ -30,6 +31,7 @@ public class StudyActivity extends BaseActivity implements View.OnClickListener 
     private Topic topic;
 
     //view
+    ImageView ivStar;
     ImageView ivBack;
     TextView tvTopicName;
     TextView tvOrigin;
@@ -91,11 +93,13 @@ public class StudyActivity extends BaseActivity implements View.OnClickListener 
         tvDetails.setOnClickListener(this);
         tvDidntKnow.setOnClickListener(this);
         tvKnew.setOnClickListener(this);
+        ivStar.setOnClickListener(this);
 
         loadData();
     }
 
     private void bindView() {
+        ivStar = findViewById(R.id.star_imageview);
         ivBack = findViewById(R.id.iv_back);
         tvTopicName = findViewById(R.id.tv_topic_name);
         tvOrigin = findViewById(R.id.tv_origin);
@@ -200,6 +204,10 @@ public class StudyActivity extends BaseActivity implements View.OnClickListener 
                 break;
             case R.id.tv_knew:
                 nextWord(true);
+                break;
+            case R.id.star_imageview:
+                DatabaseManager.getInstance(this).addWordToMyWordlist(word);
+                Toast.makeText(StudyActivity.this,"Added",Toast.LENGTH_SHORT).show();
                 break;
         }
     }
