cdcb5aa6853fedff28acbee1bfb98e2bc4fff881
1m93 <thunderking9x@gmail.com>
2019-04-30 11:26:53 +0700

    Trang quản trị admin phiên bản mới
    1. Cải tiến giao diện
    2. Sử dụng bootstrap modal để trực quan hơn
    3. Không sử dụng id trên thanh url để  tăng tính bảo mật

---
+++ NguyenTuanLinh/BooksParadiseAdmin/modules/header.php
@@ -0,0 +1,37 @@
+<header class="fixed-top">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.php"><strong>BooksParadise Admin</strong></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Trang chủ<span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="index.php?quanly=chitietsach">Sách</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="index.php?quanly=theloaisach">Thể loại</a>
+              </li>
+            </ul>
+            <ul class="navbar-nav ml-lg-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tài Khoản</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="#">Tài khoản</a>
+                      <a class="dropdown-item" href="#">Đăng nhập</a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="#">Đăng xuất</a>
+                    </div>
+                </li>
+                <form class="form-inline my-2 my-lg-0">
+                  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-primary my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </ul>
+        </div>
+    </nav>
+</header>
