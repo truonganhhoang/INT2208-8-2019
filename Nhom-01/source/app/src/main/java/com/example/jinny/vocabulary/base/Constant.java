d9b34e19931e560e512c07d7f6a6f40b19fbe7f0
kuribohkute <kuribohwithwing@gmail.com>
2019-05-07 14:37:37 +0700

    cap nhat ung dung

--- Nhom-01/source/app/src/main/java/com/example/jinny/vocabulary/base/Constant.java
+++ Nhom-01/source/app/src/main/java/com/example/jinny/vocabulary/base/Constant.java
@@ -1,50 +1,17 @@
 package com.example.jinny.vocabulary.base;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
-import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
+public class Constant {
+    //reminder
+    public static int REMINDER_REQUEST_CODE = 0;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+    //user data
+    public static String USER_DATA = "user_data";
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+    public static String USER_ID = "user_id";
 
-public abstract class BaseActivity extends AppCompatActivity {
+    //noti
+    public static int NOTI_REQUEST_CODE = 1;
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(getLayoutId());
-
-        setupUI();
-    }
-    protected abstract int getLayoutId();
-
-    protected abstract void setupUI();
-
-    private void takeKeyHash() {
-        PackageInfo info;
-        try {
-            info = getPackageManager().getPackageInfo("com.example.jinny.vocabulary", PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md;
-                md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                String something = new String(Base64.encode(md.digest(), 0));
-                //String something = new String(Base64.encodeBytes(md.digest()));
-                Log.e("hash key", something);
-            }
-        } catch (PackageManager.NameNotFoundException e1) {
-            Log.e("name not found", e1.toString());
-        } catch (NoSuchAlgorithmException e) {
-            Log.e("no such an algorithm", e.toString());
-        } catch (Exception e) {
-            Log.e("exception", e.toString());
-        }
-    }
+    //intent
+    public static String TOPIC = "topic";
 }
--- Nhom-01/source/app/src/main/java/com/example/jinny/vocabulary/base/Constant.java~
+++ Nhom-01/source/app/src/main/java/com/example/jinny/vocabulary/base/Constant.java~
@@ -1,17 +1,50 @@
 package com.example.jinny.vocabulary.base;
 
-public class Constant {
-    //reminder
-    public static int REMINDER_REQUEST_CODE = 0;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 
-    //user data
-    public static String USER_DATA = "user_data";
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-    public static String USER_ID = "user_id";
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-    //noti
-    public static int NOTI_REQUEST_CODE = 1;
+public abstract class BaseActivity extends AppCompatActivity {
 
-    //intent
-    public static String TOPIC = "topic";
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(getLayoutId());
+
+        setupUI();
+    }
+    protected abstract int getLayoutId();
+
+    protected abstract void setupUI();
+
+    private void takeKeyHash() {
+        PackageInfo info;
+        try {
+            info = getPackageManager().getPackageInfo("com.example.jinny.vocabulary", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                //String something = new String(Base64.encodeBytes(md.digest()));
+                Log.e("hash key", something);
+            }
+        } catch (PackageManager.NameNotFoundException e1) {
+            Log.e("name not found", e1.toString());
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("no such an algorithm", e.toString());
+        } catch (Exception e) {
+            Log.e("exception", e.toString());
+        }
+    }
 }
