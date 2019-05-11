7a46325faca301cb66b377dfbb194ea0f21c3901
lyminhnghia <lmn2461999@gmail.com>
2019-05-11 16:32:48 +0700

    Làm backend cho phần đăng nhập của người dùng

--- nhom-19/Quizy/config/passport.js
+++ nhom-19/Quizy/config/passport.js
@@ -107,3 +107,55 @@ module.exports = function(passport, user, userdetails) {
     )
   );
 
+  //LOCAL SIGNIN
+  passport.use(
+    "local-signin",
+    new LocalStrategy(
+      {
+        // by default, local strategy uses username and password, we will override with email
+        usernameField: "email",
+        passwordField: "password",
+        passReqToCallback: true // allows us to pass back the entire request to the callback
+      },
+
+      function(req, email, password, done) {
+        var User = user;
+        var isValidPassword = function(userpass, password) {
+          return bCrypt.compareSync(password, userpass);
+        };
+        User.findOne({
+          where: {
+            email: email
+          }
+        })
+          .then(function(user) {
+            if (!user) {
+              return done(
+                null,
+                false,
+                req.flash("error", "Email này không tồn tại")
+              );
+            }
+
+            if (!isValidPassword(user.password, password)) {
+              return done(
+                null,
+                false,
+                req.flash("error", "Mật khẩu không chính xác")
+              );
+            }
+
+            var userinfo = user.get();
+            return done(null, userinfo);
+          })
+          .catch(function(err) {
+            console.log("Error:", err);
+
+            return done(null, false, {
+              message: "Something went wrong with your Signin"
+            });
+          });
+      }
+    )
+  );
+};
