c760817a8751c93a361df6da2c3782a43bf941d0
thanhvh99 <vuhathanh00@gmail.com>
2019-05-11 18:20:42 +0700

    Cập nhật chức năng liên hệ

--- nhom-19/Quizy/routes/index.js
+++ nhom-19/Quizy/routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var models = require("../models");
 const Contact = models.contact;
+const nodemailer = require("nodemailer");
 /* GET home page. */
 router.get("/", function(req, res, next) {
   console.log(req.isLogged);
@@ -11,16 +12,45 @@ router.get("/", function(req, res, next) {
   });
 });
 
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'vatliyeutoi@gmail.com',
+    pass: 'hoangthithuytrang12345'
+  }
+});
+
+
+
 router.post("/contact", function(req, res) {
   const contact = req.body;
   console.log(contact);
-  Contact.create(contact).then(contact => {
-    if (contact) {
-      res.send({ success: true });
+  var mailOptionsForDev = {
+    from: 'vatliyeutoi@gmail.com',
+    to: 'vatliyeutoi@gmail.com',
+    subject: 'Quizy Contact' + contact.name,
+    text: JSON.stringify(contact)
+  };
+
+  var mailOptionsForUser = {
+    from: 'vatliyeutoi@gmail.com',
+    to: contact.email,
+    subject: 'Quizy Contact',
+    text: 'Cảm ơn phản hồi của bạn.\nMong bạn sẽ tiếp tục ủng hộ Quizy trong tương lai.'
+  };
+  
+  transporter.sendMail(mailOptionsForDev, function(error, info){
+    if (error) {
+      console.log(error);
     } else {
-      res.send({
-        success: false
-      });
+      console.log('Email sent: ' + info.response);
+    }
+  });
+  transporter.sendMail(mailOptionsForUser, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent for User: ' + info.response);
     }
   });
 });
