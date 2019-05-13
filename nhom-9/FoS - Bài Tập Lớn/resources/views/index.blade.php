8b3a15966b6edcdb6ff6c35ba460480fcfa21b5a
Nguyen Huu Minh Quang <36332303+quangnguyen99@users.noreply.github.com>
2019-05-13 17:07:56 +0700

    sửa lỗi môn học

--- nhom-9/FoS - Bài Tập Lớn/resources/views/index.blade.php
+++ nhom-9/FoS - Bài Tập Lớn/resources/views/index.blade.php
@@ -2,33 +2,7 @@
 
 @section('main')
 
- {{--    @if (!is_null($purchased_courses))
-    <div class="panel panel-info">
-      <div class="panel-heading"><h4><strong>Khóa học</strong></h4></div>
-    </div>
-        <div class="row">
-
-        @foreach($purchased_courses as $course)
-            <div class="col-sm-4 col-lg-4 col-md-4">
-                <div class="thumbnail">
-                    <img src="{{ asset('uploads/'.$course->course_image) }}"></a>
-                    <div class="caption">
-                        <h4><a href="{{ route('courses.show', [$course->slug]) }}">{{ $course->title }}</a>
-                        </h4>
-                        <p>{{ $course->description }}</p>
-                    </div>
-                    <div class="ratings">
-                        <p>Progress: {{ Auth::user()->lessons()->where('course_id', $course->id)->count() }}
-                            of {{ $course->lessons->count() }} lessons</p>
-                    </div>
-                </div>
-            </div>
-        @endforeach
-        </div>
-        <hr />
-
-    @endif --}}
-
+{{- Hiển thị khóa học các môn học của đề thi-}}
     <div class="panel panel-info">
       <div class="panel-heading"><h4><strong>Tất cả đề thi</strong></h4></div>
     </div>
@@ -36,6 +10,9 @@
     @foreach($courses as $course)
         <div class="col-sm-4 col-lg-4 col-md-4">
             <div class="thumbnail">
+            
+            {{- Hiển thị hình ảnh khóa học các môn học của đề thi-}}
+            
                <img src="{{ asset('uploads/'.$course->course_image) }}"></a>
                 <div class="caption">
                     {{-- <h4 class="pull-right">${{ $course->price }}</h4> --}}
@@ -44,7 +21,6 @@
                     <p>{{ $course->description }}</p>
                 </div>
                 <div class="ratings">
-                    {{-- <p class="pull-right">Học viên: {{ $course->students()->count() }}</p> --}}
                     <p>
                         @for ($star = 1; $star <= 5; $star++)
                             @if ($course->rating >= $star)
