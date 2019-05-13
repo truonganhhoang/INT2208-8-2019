9af8cdf7c383a191337e47f1dec178e0c391bf7d
Nguyen Huu Minh Quang <36332303+quangnguyen99@users.noreply.github.com>
2019-05-13 17:09:04 +0700

    update môn học của khóa học

--- nhom-9/FoS - Bài Tập Lớn/resources/views/index.blade.php
+++ nhom-9/FoS - Bài Tập Lớn/resources/views/index.blade.php
@@ -2,7 +2,6 @@
 
 @section('main')
 
-{{- Hiển thị khóa học các môn học của đề thi-}}
     <div class="panel panel-info">
       <div class="panel-heading"><h4><strong>Tất cả đề thi</strong></h4></div>
     </div>
@@ -12,14 +11,15 @@
             <div class="thumbnail">
             
             {{- Hiển thị hình ảnh khóa học các môn học của đề thi-}}
-            
                <img src="{{ asset('uploads/'.$course->course_image) }}"></a>
+               
                 <div class="caption">
-                    {{-- <h4 class="pull-right">${{ $course->price }}</h4> --}}
+                {{- Hiển thị khóa học các môn học của đề thi-}}
                     <h4><a href="{{ route('courses.show', [$course->slug]) }}">{{ $course->title }}</a>
                     </h4>
                     <p>{{ $course->description }}</p>
                 </div>
+                
                 <div class="ratings">
                     <p>
                         @for ($star = 1; $star <= 5; $star++)
